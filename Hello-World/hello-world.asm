69cbea6aceef592c4fb51e181ba110f5fbc19350
S Pradeep Kumar <gohanpra@gmail.com>
2012-10-13 21:35:17 +0530

    Added comments, README, and Final Upload folder for Merge Sort.

--- Hello-World/hello-world.asm
+++ Hello-World/hello-world.asm
@@ -6,11 +6,27 @@
 	      .DATA
 	      hello_world_str db	"Yo, boyz! I am sing song", 10, "Hello, world!", 0
 	      yo_boyz_str db	"Yo, boyz!", 10, "Yo, boyz!", 0
+	      arr   dd 1, 2, 3, 4, 5
 	      .UDATA
 	      .CODE
 	      .STARTUP
 HELLO_WORLD:
 	      PutStr hello_world_str
 	      nwln
+
+	      mov   EAX, arr
+	      mov   ECX, 5
+	      sub   EDI, EDI
+loop1:
+	      ;; mov   DWORD  [arr + 4], 3
+	      ;; mov   DWORD  [EAX + 4], 3
+	      PutLInt [arr + EDI * 4]
+	      PutLInt [EAX + EDI * 4]
+	      inc EDI
+	      loop loop1
+	      ;; PutLInt [arr + 4]
+	      ;; PutLInt [arr + 8]
+	      ;; PutLInt [arr + 12]
+	      ;; PutLInt [arr + 16]
 	      call YO_BOYZ
 	      .EXIT
